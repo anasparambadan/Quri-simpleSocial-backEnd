b756ec7c62d2322655c976fb008cd73087280342
anas <forandro002@gmail.com>
2023-04-17 14:34:42 +0530

    changed index.js to index.cjs

--- index.cjs
+++ index.cjs
@@ -13,8 +13,8 @@ import cors from 'cors'
 const app = express()
 
 //Public resource folder
-app.use(express.static(__dirname + '/tmp'))
-app.use('/tmp',express.static("tmp"))
+app.use(express.static(__dirname,'/tmp'))
+app.use('/temp',express.static("tmp"))
 
 
 //middleWares
