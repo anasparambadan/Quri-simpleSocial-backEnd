295c90cc7d679239e17bb4eddf9148b28fd3ee74
anas <forandro002@gmail.com>
2023-04-18 11:44:58 +0530

    toast

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,10 +45,10 @@ export const loginUser = async (req, res) => {
         if (user) {
             const passwordValidity = await bcrypt.compare(password, user.password)
             if (!passwordValidity) {
-                res.status(400).json({ message: "Wrong Password" })
+                res.status(200).json({ message: "Wrong Password" })
             }
             else {
-                const token = jwt.sign({
+                const token = jwt.sign({    
                     email: user.email, id: user._id
                 }, process.env.JWT_KEY, { expiresIn: "1h" })
                 
@@ -56,7 +56,7 @@ export const loginUser = async (req, res) => {
             }
         }
         else {
-            res.status(404).json({ message: "User not found" })
+            res.status(200).json({ message: "User not found" })
         }
     } catch (error) {
         res.status(500).json({ message: error.message })
