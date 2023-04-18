f1b1035e3dc94a7a2336b6e065af27c5ae7f1442
anas <forandro002@gmail.com>
2023-04-18 12:15:11 +0530

    package.json start node

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ const app = express()
 
 //Public resource folder
 app.use(express.static('public'))
-app.use('/images',express.static("images"))
+app.use('/images', express.static("images"))
 
 
 //middleWares
 dotenv.config()
-app.use(bodyParser.json({extended:true,limit: '50mb'}))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json({ extended: true, limit: '50mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 
 
-const port = process.env.PORT || 5000 
-mongoose.connect(process.env.MONGO_DB,{useNewURLParser:true, useUnifiedTopology:true}).then(()=>app.listen(port,()=>console.log(`node server started at ${port}`))).catch((error)=>console.log(error))
+const port = process.env.PORT || 5000
+mongoose.connect(process.env.MONGO_DB, { useNewURLParser: true, useUnifiedTopology: true }).then(() => app.listen(port, () => console.log(`node server started at ${port}`))).catch((error) => console.log(error))
 
 
 //usage of routes
 
-app.use('/auth',authRoute )
-app.use('/post',postRoute)
-app.use('/upload',uploadRoute)
+app.use('/auth', authRoute)
+app.use('/post', postRoute)
+app.use('/upload', uploadRoute)
